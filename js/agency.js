b5a6abadc8f4aa80a88e70365c14ce9de4f80592
kwonars-student <kwonars@yonsei.ac.kr>
2022-12-28 04:21:39 +0900

    Web scroll option change

--- js/agency.js
+++ js/agency.js
@@ -10,7 +10,7 @@ $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 2500, 'easeInOutExpo');
+        }, 1000, 'easeInOutExpo');
         event.preventDefault();
     });
 });
