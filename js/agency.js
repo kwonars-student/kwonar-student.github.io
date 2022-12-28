5bd985e73f1b15534640f0e752bf3bfb26966b89
kwonars-student <kwonars@yonsei.ac.kr>
2022-12-29 02:46:03 +0900

    Portfolio update

--- js/agency.js
+++ js/agency.js
@@ -10,7 +10,7 @@ $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 10000, 'easeInOutQuad');
+        }, 800, 'easeInOutQuad');
         event.preventDefault();
     });
 });
